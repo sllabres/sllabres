3254229868e424d0e4a6770530642c1413819ef4
sllabres <sllabres@gmail.com>
2012-07-11 20:01:03 +0100

    add webgl default test page and follow sample

---
+++ sllabres.web/Areas/WebGl/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sllabres.web.Areas.WebGl.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Follow()
+        {
+            return View();
+        }
+    }
+}
