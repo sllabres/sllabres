3da8c74d37ba3a5f29dd6f071a5e8988e91cabba
Seb <sllabres@gmail.com>
2012-09-27 21:37:06 +0100

    rake build, test and commit

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,22 @@
+require 'albacore'
 task :default => [:git_commit_and_push]
 
-task :git_commit_and_push do
+desc "Build Solution"
+msbuild :build do |msb|
+	msb.properties :configuration => :Release
+	msb.targets :Clean, :Build
+	msb.solution = "sllabres.sln"
+end
+
+desc "Run unit tests"
+nunit :test => :build do |nunit|
+	nunit.command = "../Library/NUnit/bin/nunit-console.exe"
+	nunit.assemblies "./Sllabres.Web.Test/Sllabres.Web.Test.csproj"
+	nunit.options '/xml=Sllabres.Tests-Results.xml'
+end
+
+
+task :git_commit_and_push => :test do
 	comment = ask('Enter Comment: ')
 	`git add .`
 	`git commit -m "#{comment}"`
