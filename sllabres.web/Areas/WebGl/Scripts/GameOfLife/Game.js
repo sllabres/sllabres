efc03d1ea3bffb7ba313850b65927144c6d424e3
Seb <sllabres@gmail.com>
2012-10-02 23:23:01 +0100

    Automated Commit

--- sllabres.web/Areas/WebGl/Scripts/GameOfLife/Game.js
+++ sllabres.web/Areas/WebGl/Scripts/GameOfLife/Game.js
@@ -1,6 +1,7 @@
+/// Disclaimer: Ashamed to say that in my haste this code was not test driven :(
 var drawable;
 var drawService;
-var gridSize = 1000
+var gridSize = 500
 var gridWidth = Math.sqrt(gridSize);
 
 function init() {
