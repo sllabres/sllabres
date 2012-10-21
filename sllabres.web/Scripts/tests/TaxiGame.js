86aea47a920a91c6247b1f48fb5279681510cd8a
Seb <sllabres@gmail.com>
2012-10-21 19:30:17 +0100

    Automated Commit

--- sllabres.web/Scripts/tests/TaxiGame.js
+++ sllabres.web/Scripts/tests/TaxiGame.js
@@ -1,19 +1,34 @@
 (function() {
-	var Game = function() {
+	var Game = function(requestAnimationFrame) {
 		var run = function() {
-
+			requestAnimationFrame(run);
 		}
+
+		return {
+			run : run
+		};
 	}
 
 	module("Game Tests");
-	test("Running game run calls request animation frame to handle updates", function() {  
-		/*
+	test("Running game calls request animation frame", function() {  
+		var requestAnimationFrameCalled = false;		
 		var requestAnimationFrame = function() {
-
+			requestAnimationFrameCalled = true;
 		};
 		
-		//var game = new Game(renderer);  
-		game.run();*/
-	    ok(true);
+		var game = new Game(requestAnimationFrame);
+		game.run();
+	    equal(requestAnimationFrameCalled, true);
+	});
+
+	test("Running game calls request animation frame with run", function() {
+		var methodPassed;
+		var requestAnimationFrame = function(method) {
+			methodPassed = method;
+		};
+
+		var game = new Game(requestAnimationFrame);
+		game.run();
+		equal(methodPassed, game.run);
 	});
 })();
